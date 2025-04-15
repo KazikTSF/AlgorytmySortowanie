43aee076ad8643f2abe29be0dbf76b5435d892ab
Patryk Kazmierczak <github.scarcity013@passsmail.net>
2025-04-16 00:06:52 +0200

    add initial implementation of QuickSort class and .clang-format configuration

--- src/Quicksort.hpp
+++ src/Quicksort.hpp
@@ -1,3 +1,32 @@
 #ifndef QUICKSORT_HPP
 #define QUICKSORT_HPP
-#endif //QUICKSORT_HPP
+template <class T>
+class QuickSort
+{
+public:
+  void partition(T* arr, const int size) const {
+    int pivot = arr[0];
+
+    int i = -1;
+    int j = size;
+    while (true) {
+
+      do {
+        i++;
+      } while (arr[i] < pivot);
+
+      do {
+        j--;
+      } while (arr[j] > pivot);
+
+      if (i > j) break;
+
+      swap(arr[i], arr[j]);
+    }
+  }
+    void quickSort(T* array, const int size)
+    {
+
+    }
+};
+#endif // QUICKSORT_HPP
